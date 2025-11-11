1dc27071f83222c52f8be8cd901a4a5ba240bc98
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-11 21:35:18 +1100

    Add files via upload

---
+++ 667a/main.py
@@ -0,0 +1,36 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from math import pi
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+d, h, v, e = MII()
+
+init = (d / 2) * (d / 2) * pi
+
+inc = init * e
+if inc >= v:
+    print("NO")
+else:
+    print("YES")
+    print(init * h / (v - inc) )
+
