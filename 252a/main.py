aa18726505c013eef595c3469752066f98ab68fd
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-13 21:04:26 +1100

    Add files via upload

---
+++ 252a/main.py
@@ -0,0 +1,37 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+b = II()
+L = LII()
+
+ans = 0
+
+
+for i in range(b):
+    tmp = 0
+    for j in range(i, b):
+        tmp ^= L[j]
+        ans = max(ans, tmp)
+print(ans)
---
+++ 884b/main.py
@@ -0,0 +1,29 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+n, x = MII()
+L = LII()
+k = sum(L)
+print("YES" if k + n - 1 == x else "NO")
