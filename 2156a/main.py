72f5b47f5776e7af7f00dc4955261ed61b4e841b
SsyHow <163735878+SsyHow@users.noreply.github.com>
2025-11-15 22:14:53 +1100

    Add files via upload

---
+++ 2156a/main.py
@@ -0,0 +1,32 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+a = II()
+for _ in range(a):
+    b = II()
+    if b & 1 == 0:
+        print(b // 2 - 1)
+    else:
+        print(b // 2)
---
+++ 2167d/main.py
@@ -0,0 +1,39 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+
+def I():
+    return input()
+
+
+def II():
+    return int(input())
+
+
+def MII():
+    return map(int, input().split())
+
+
+def LI():
+    return list(input().split())
+
+
+def LII():
+    return list(map(int, input().split()))
+from math import gcd
+# alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+L = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53]
+def solve():
+    b = II()
+    K = LII()
+    for i in L:
+        for j in K:
+            if gcd(i, j) == 1:
+                return i
+    return -1
+
+a = II()
+for _ in range(a):
+    print(solve())
